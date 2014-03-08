c6f24a5d73adf254c35d566c496ede829e38f5c5
anhquan <info@anhquan.de>
2014-03-08 01:25:35 +0100

    Compile the report also

--- kassebericht/src/main/java/de/anhquan/ireport/ReportGenerator.java
+++ kassebericht/src/main/java/de/anhquan/ireport/ReportGenerator.java
@@ -8,6 +8,7 @@ import java.util.Locale;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
@@ -25,17 +26,36 @@ public class ReportGenerator {
 	private static final String OUTPUT_JASPER_REPORT = "report.pdf";
 	private static final String COMPILED_JASPER_REPORT = "report.jasper";
 
-	public static void main(String[] args) throws FileNotFoundException,
-			JRException {
+	public static void main(String[] args)  {
 
-		ReportGenerator reporter = new ReportGenerator();
-
+		try {
+			new ReportGenerator().execute();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void execute() throws FileNotFoundException,	JRException{
+		compileReport();
+		
 		System.out.println("Filling report ...");
-		reporter.fillReport(getDataSource(EXPORT_FINANCISTO_CSV));
+		fillReport(getDataSource(EXPORT_FINANCISTO_CSV));
 
 		System.out.println("Done!");
 	}
 
+	public void compileReport(){
+		try {
+			System.out.println("Compiling file 'report.jrxml' to report.jasper ...");
+			JasperCompileManager.compileReportToFile("report.jrxml",
+			        "report.jasper");
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+
+	}
 	public void fillReport(JRDataSource dataSource) throws JRException {
 		JasperPrint jasperPrint = JasperFillManager.fillReport(COMPILED_JASPER_REPORT,
 				new HashMap<String, Object>(), dataSource);
